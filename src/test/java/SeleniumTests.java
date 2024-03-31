cb7a53115f64ca18da1e6641a48128259f5a733e
markinads <markinads@yandex.ru>
2024-03-31 21:59:08 +0300

    selenium basic test

---
+++ src/test/java/SeleniumTests.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("selenium")
+class SeleniumTests {
+    final String TEST_URL = "https://mipt.ru/";
+    WebDriver driver;
+
+    @BeforeEach
+    void init() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void close() {
+        driver.close();
+    }
+
+    @Test
+    void SeleniumBasicTest() {
+        driver.get(TEST_URL);
+        assertEquals("МФТИ — Московский физико-технический институт", driver.getTitle());
+    }
+}
